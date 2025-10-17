7e80e66f4e80bbb731ea2a5b38cbd5dc2d60e513
Newt_Louis <l.ngoc.17395@gmail.com>
2025-10-18 00:10:28 +0700

    fix error when build project with maven not understand system enviroment variables of testing phase

--- src/test/java/ngoc/connect_gemini_api/ConnectGeminiApiApplicationTests.java
+++ src/test/java/ngoc/connect_gemini_api/ConnectGeminiApiApplicationTests.java
@@ -2,14 +2,8 @@ package ngoc.connect_gemini_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-        "GOOGLE_GENAI_USE_VERTEXAI=True",
-        "GOOGLE_CLOUD_PROJECT=spring-boot-smart-blog", // Thay bằng Project ID của bạn
-        "GOOGLE_CLOUD_LOCATION=us-central1"
-})
 class ConnectGeminiApiApplicationTests {
 
 	@Test
